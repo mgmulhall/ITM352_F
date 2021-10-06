58788aaf3b34cdabee5176bf989dce2925b1d227
mgmulhall <margaretmulhall00@gmail.com>
2021-10-05 22:35:14 -1000

    SmartPhoneProducts2

---
+++ Lab 8/Ex1.js
@@ -0,0 +1,15 @@
+require("./products_data.js");
+
+var num_products = 5;
+
+var count = 1;
+while(count <= num_products) {
+    if(count > num_products/2) {
+        console.log("That's enough!");
+        break;
+    }
+    console.log(`${count}. ${eval('name'+ count)}`);
+    count++;
+}
+console.log("That's all we have!");
+
