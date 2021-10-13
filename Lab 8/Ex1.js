8c6e75981103874008eeea185de38bc26e1ad93a
mgmulhall <margaretmulhall00@gmail.com>
2021-10-13 13:19:13 -1000

    Lab10

--- Lab 8/Ex1.js
+++ Lab 8/Ex1.js
@@ -1,15 +1,12 @@
 require("./products_data.js");
 
 var num_products = 5;
-
-var count = 1;
-while(count <= num_products) {
-    if(count > num_products/2) {
+for (var count = 1; eval("typeof name"+count) != 'undefined'; count++) {
+    if (count > num_products/2) {
         console.log("That's enough!");
-        break;
+        process.exit();
     }
-    console.log(`${count}. ${eval('name'+ count)}`);
-    count++;
+    console.log(`${count}. ${eval('name' + count)}`);
 }
-console.log("That's all we have!");
+
 
