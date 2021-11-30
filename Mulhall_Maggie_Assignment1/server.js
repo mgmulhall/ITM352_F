3bdf35f8d2d81caf106d4e88dca40a2fb2682f69
mgmulhall <margaretmulhall00@gmail.com>
2021-11-30 13:39:49 -1000

    Assignment 1

--- Mulhall_Maggie_Assignment1/server.js
+++ Mulhall_Maggie_Assignment1/server.js
@@ -1,54 +1,71 @@
 var express = require('express');
 var app = express();
 var myParser = require("body-parser");
-var fs = require('fs');
-var data = require('./product_data.js');
+//var fs = require('fs');
+var data = require('./public/product_data.js');
 var products = data.products;
 
+//connect server to all files in directory 
 app.all('*', function (request, response, next){
     console.log(request.method + 'to' + request.path);
     next();
 });
 
+// Handle request for any file in public
+app.use(express.static('./public'));
+//validation
+
+//check that there is a value in the text box on index
+function checkQuantityTextbox(theTextbox) {
+    errors = isNonNegInt(theTextbox.value, true);
+    if (errors.length == 0) errors = ['You want:']; //if there are no errors, "You want:*requested amount*
+    if (theTextbox.value.trim() == '') errors = ['Please type quantity desired: '];// if there are no requested amounts, ask for amount
+}  
+// check if the value entered if valid and in stock
+function isNonNegInt(inputstring, returnErrors = false) {
+    errors = []; // assume no errors at first
+    if(Number(inputstring) != inputstring) {
+        errors.push('<font color="red">Not a Number!</font>'); // Check if string is a number, if not say "Not a number!"
+    }
+    else
+    {
+        if (inputstring ==0) errors.push('<font color="red">You Need More Than That!</font>');
+        if(inputstring < 0) errors.push('<font color="red">Negative Value!</font>'); // Check if it is non-negative, if not say "Negative value!"
+        if(parseInt(inputstring) != inputstring) errors.push('<font color="red">Not an integer!</font>'); // Check that it is an integer, if not say "Not an integer!"
+        if (inputstring > products[i].quantity_availaible) errors.push('<font color="red">Not Enough In Stock!</font>'); // check if the requested amount is in stock
+    }          
+    return returnErrors ? errors : (errors.length == 0); // if there are no errors , errors.length ==0, proceed to next validation
+}
+
+
 app.use(myParser.urlencoded({ extended: true }));
-app.post("/process_form", function (request,response){
-    //process_quantity_form(request.body, response);
+
+// if there are no errors, display inovice table when you click purchase
+function check_out()
+if (errors.length = 0) 
+app.post("/process_form", function (request, response){
+    display_invoice_table_rows(request.body, response);
     response.send(request.body);
 });
 
-app.use(express.static('./public'));
-
-// Route to handle any request; also calls next
-app.all('*', function (request, response, next) {
-    console.log(request.method + ' to path: ' + request.path);
-    next();
-});
-
-// Route to handle just the path /test
-app.get('/test', function (request, response, next) {
-    response.send('Got a GET request to path: test');
-});
-
-app.use(myParser.urlencoded({ extended: true }));
-
-// Rule to handle process_form request from order_page.html
-/*app.post("/process_form", function (request, response) {
-    let POST = request.body;
-    let type = products[0]['type'];
-    let type_price = products[0]['price'];
-
-    if (typeof POST['quantity_textbox'] != 'undefined') {
-        let quantity = POST['quantity_textbox'];
-        if (isNonNegativeInteger(quantity)) {
-            products[0]['total_sold'] += Number(quantity);
-            response.send(`<H2>Thank you for ordering ${quantity} ${type}! Your total is \$${quantity * type_price}.</H2>`);
-        } else {
-            response.send(`<I>${quantity} is not a valid quantity!</I>`);
+//loop for invoice table if input passed both validations
+function display_invoice_table_rows(inputstring) {
+    inputstring= a_qty; 
+    subtotal = 0;
+    str = '';
+        if (a_qty != undefined) {// if the requested quantity is valid
+            extended_price =a_qty * products[i].price //multiply the price by quantity and make it the extended price
+            subtotal += extended_price; // subtotal is added to and is now extended price
+        //make actual table
+            str += (`
+  <tr>
+    <td width="43%">${products[i].item}</td>
+    <td align="center" width="11%">${a_qty}</td>
+    <td width="13%">\$${products[i].price}</td>
+    <td width="54%">\$${extended_price}</td>
+  </tr>
+  `);
         }
-    }
-});*/
+    };
 
-// Handle request for any static file
-app.use(express.static('./public'));
-
-//app.listen(8080,()=> console.log(`listening on port 8080`));
+app.listen(8080,()=> console.log(`listening on port 8080`));
